3720fce22b1fd25ef6c6665d6ac50f19fae6f979
nick <nickgonzales76017@gmail.com>
2022-06-13 23:38:10 -0500

    n

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => ({
       password: env('DATABASE_PASSWORD', '@Lotion1201475369'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', true),
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
     },
     debug: false,
